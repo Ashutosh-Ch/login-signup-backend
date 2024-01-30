de24e5c22009183fadff6181fb3746d15328f039
Ashutosh Chaubey <ashutoshchaubeymail@gmail.com>
2024-01-31 01:23:05 +0530

    fourth

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const corsOptions ={
 }
 app.use(cors(corsOptions))
 
-app.get("/",cors(),(req,res)=>{
-    
-})
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
 
 app.post("/login",async(req,res)=>{
     const{email,password} = req.body
