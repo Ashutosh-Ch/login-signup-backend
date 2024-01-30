532f201222d1e12718ada8ab5c04a4f33af29968
Ashutosh Chaubey <ashutoshchaubeymail@gmail.com>
2024-01-31 01:24:46 +0530

    fifth

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const corsOptions ={
-    credentials:true
-}
+
 app.use(cors(corsOptions))
 
 router.get("/", (req, res) => {
